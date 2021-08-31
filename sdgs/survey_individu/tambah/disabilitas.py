89f29c41b4c4d4c9756f5e024bbdf42fa3bfcde1
hexatester <revolusi147id@gmail.com>
2021-08-31 12:36:48 +0700

    Update Disabilitas mapping

--- sdgs/survey_individu/tambah/disabilitas.py
+++ sdgs/survey_individu/tambah/disabilitas.py
@@ -2,25 +2,16 @@ import attr
 from typing import Dict
 
 MAPPING = {
-    "1": "2",
-    "2": "2",
-    "3": "2",
-    "4": "2",
-    "5": "2",
-    "6": "2",
-    "7": "2",
-    "8": "2",
-    "9": "2",
-    "10": "2",
-    "11": "2",
-    "12": "2",
-    "13": "2",
-    "14": "2",
-    "15": "2",
-    "16": "2",
-    "17": "2",
-    "18": "2",
-    "19": "2",
+    "1": "tunanetra",
+    "2": "tunarungu",
+    "3": "tunawicara",
+    "4": "tunarungu_wicara",
+    "5": "tunadaksa",
+    "6": "tunagrahita",
+    "7": "tunalaras",
+    "8": "cacat_eks_kusta",
+    "9": "cacat_ganda",
+    "10": "dipasung",
 }
 
 
