7ef2902c10371782f25add0b993c5936467ff742
hexatester <revolusi147id@gmail.com>
2021-09-01 20:34:38 +0700

    Added .bu

--- sdgs/survey_individu/tambah/status_pernikahan.py
+++ sdgs/survey_individu/tambah/status_pernikahan.py
@@ -6,3 +6,14 @@ class StatusPernikahan(Enum):
     KAWIN = "2"
     CERAI_HIDUP = "3"
     CERAI_MATI = "4"
+
+    @property
+    def bu(self) -> bool:
+        """Mengecek apa membutuhkan usia menikah"""
+        if self.value == self.KAWIN:
+            return True
+        elif self.value == self.CERAI_HIDUP:
+            return True
+        elif self.value == self.CERAI_MATI:
+            return True
+        return False
